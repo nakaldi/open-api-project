70f43efef37aa8ed93830b7626d7f060444c706e
Kim Beomhee <nakaldi@gmail.com>
2025-11-24 17:51:56 +0900

    Init Project

---
+++ src/test/java/com/example/open_api_project/OpenApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.open_api_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
